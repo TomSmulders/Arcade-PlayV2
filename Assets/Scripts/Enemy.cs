c5b0a613ea76469a1fd2a52eff9532e72219ebca
TomSmulders <91885901+TomSmulders@users.noreply.github.com>
2022-12-22 23:04:52 +0100

    menuButtons shance

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -35,6 +35,7 @@ public class Enemy : MonoBehaviour
         var step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
         }
+        
         //enemy shooting invoke
         if (enemyCanShoot)
         {
--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -5,11 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour
 {
-    int currentSelected = 1;
-    public int max = 3;
 
-    public int selected1;
-    public int selected2;
 
 
     void Start()
@@ -20,38 +16,17 @@ public class MenuButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(SceneManager.GetActiveScene().name == "start")
-        {
-            if (Input.GetKeyDown(KeyCode.W) && currentSelected >= 1)
-            {
-                currentSelected--;
-                RectTransform myRectTransform = GetComponent<RectTransform>();
-                myRectTransform.localPosition += new Vector3(0,155,0);  
-            }
-            if (Input.GetKeyDown(KeyCode.S) && currentSelected <= max - 2)
-            {
-                currentSelected++;
-                RectTransform myRectTransform = GetComponent<RectTransform>();
-                myRectTransform.localPosition -= new Vector3(0, 155, 0);
-            } 
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                switch (currentSelected)
-                {
-                    case 0:
-                        SceneManager.LoadScene(selected1); 
-                        break;
-                    case 1:
-                        SceneManager.LoadScene(selected2);
-                        break;
-                    case 2:
-                        Application.Quit();
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
+        
     }
+
+    public void LoadScene(int sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
 }
